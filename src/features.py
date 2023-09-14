a8f66d4f010628153ce39b57b9a1e97560d57fc4
davidinations <david19002@mail.unpad.ac.id>
2023-09-14 19:13:41 +0700

    Update

--- src/features.py
+++ src/features.py
@@ -4,10 +4,6 @@ import pyinputplus as pypi
 
 path = "D:\\Users\\david\\Onedrive\\Documents\\Project\\Application\\Education\\Python\\RentACar\\data\\data.csv"
 
-def transition_add():
-    main.add_identity()
-    top.destroy()
-
 def add_validate(data_nik):
     global path
     primary_keys = []
@@ -26,15 +22,31 @@ def add_validate(data_nik):
 
 def add_data(data_nik, data_nama, data_mobil, data_harga, data_tanggal_masuk, data_status, data_tanggal_keluar):
     global path
-    rows = [[data_nik, data_nama, data_mobil, data_harga, data_tanggal_masuk, data_status, data_tanggal_keluar]]
+    rows = [[data_nik, data_nama, data_mobil, data_tanggal_masuk, data_tanggal_keluar, data_harga, data_status]]
 
-    with open(path, 'a') as file:
+    with open(path, 'a', newline='\n') as file:
         exampleWriter = csv.writer(file, delimiter=';')
         exampleWriter.writerows(rows)
 
-def show():
-    print("Show")
-    
+def show_full_data(my_tree):
+    count = 1
+    # add sample data to the Treeview widget
+    with open(path, 'r') as file:
+            exampleReader = csv.reader(file, delimiter=";")
+            for row in exampleReader:
+                my_tree.insert(parent='', index='end', iid=count, text=count, values=(row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
+                count += 1    
+
+def show_validate():
+    with open(path, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        row_count = sum(1 for row in reader)
+
+    if row_count > 0:
+        return True
+    else:
+        return False
+
 def edit():
     print("Edit")
 
--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from tkinter import *
 from tkinter import ttk
 from tkinter.messagebox import showinfo
 from tkinter import messagebox
-
+from tkcalendar import DateEntry
 
 # Function to create GUI
 def transition():
@@ -14,20 +14,20 @@ def transition():
         top_frame.grid_forget()
         main_menu()
 
-def main_menu():                # Semua Menu dan Exit
-        global top_frame, bottom_frame
+def main_menu():               
+        global top_frame, bottom_frame, mid_frame, tree_frame
         
         # create a top frames
-        top_frame = Frame(root, width=1175, height=500, bg='grey')
-        top_frame.grid(row=0, column=0, padx=50, pady=50)
+        top_frame = Frame(root, width=1100, height=500, bg='grey')
+        top_frame.grid(row=0, column=0, padx=40, pady=50)
 
         # create a bottom frames
         bottom_frame = Frame(root, width=1175, height=100, bg='grey')
-        bottom_frame.grid(row=1, column=0, padx=50, pady=10)
+        bottom_frame.grid(row=1, column=0, padx=40, pady=10)
 
         # Add a label to the top frame
         label_selamat_datang = Label(top_frame, text="Selamat Datang di Aplikasi Rental Mobil", bg='grey', font=("Helvetica", 20), height=7)
-        label_selamat_datang.grid(row=0, column=0, padx=340, pady=100)
+        label_selamat_datang.grid(row=0, column=0, padx=355, pady=100)
         
         # add 5 buttons to the bottom frame
         button_show = Button(bottom_frame, text="Show", width=30, height=3, command=show_menu)
@@ -41,34 +41,34 @@ def main_menu():                # Semua Menu dan Exit
         button_exit = Button(bottom_frame, text="Exit", width=30, height=3, command=exit_menu)
         button_exit.grid(row=0, column=4, padx=10, pady=10)
 
-def add_menu():                 # Input Primary Key dan Exit
+def add_menu():                
         global top_frame, bottom_frame, entry_nik, button_validate
         
         top_frame.grid_forget()
         bottom_frame.grid_forget()
 
         top_frame = Frame(root, width=1175, height=50, bg='grey')
-        top_frame.grid(row=0, column=0, padx=50, pady=30)
+        top_frame.grid(row=0, column=0, padx=45, pady=30)
 
         bottom_frame = Frame(root, width=1175, height=500, bg='grey')
-        bottom_frame.grid(row=1, column=0, padx=50, pady=20)
+        bottom_frame.grid(row=1, column=0, padx=60, pady=20)
 
         # create a label identitas + entry identitas
-        label_nik = Label(top_frame, text="NIK", bg='grey', width=10)
+        label_nik = Label(top_frame, text="NIK", bg='grey', width=10, font=("Helvetica", 20))
         label_nik.grid(row=0, column=0, padx=10, pady=10)
 
         entry_nik = Entry(top_frame, width=100)
         entry_nik.grid(row=0, column=1, padx=30, pady=10)
 
         # add button after entry nik to validate
-        button_validate = Button(top_frame, text="Validate", width=10, height=1, command=get_entry_nik)
+        button_validate = Button(top_frame, text="Validate", width=10, height=1, command=add_get_entry_nik)
         button_validate.grid(row=0, column=2, padx=10, pady=10)
 
         # add button after validate to exit menu
         button_exit = Button(top_frame, text="Exit", width=10, height=1, command=transition)
         button_exit.grid(row=0, column=3, padx=10, pady=10)
 
-def get_entry_nik():
+def add_get_entry_nik():
         global entry_nik
         data_nik = entry_nik.get()
         ft.add_validate(data_nik)       
@@ -80,14 +80,14 @@ def get_entry_nik():
                 pesan = f"Data dengan NIK {data_nik} sudah ada"
                 showinfo(title="Warning",message=pesan)
 
-def add_identity():             # Input Nama, Mobil, Harga, Tanggal Masuk, Status, Tanggal Keluar, dan Exit
-        global entry_nama, entry_mobil, entry_harga, entry_tanggal_masuk, entry_status, entry_tanggal_keluar, button_validate
+def add_identity():            
+        global entry_nik, entry_nama, entry_mobil, entry_harga, entry_tanggal_masuk, entry_status, entry_tanggal_keluar, button_validate
 
         button_validate.grid_forget()
 
         # add button after validate to exit menu
         button_exit = Button(top_frame, text="Exit", width=10, height=1, command=transition)
-        button_exit.grid(row=0, column=3, padx=110, pady=10)
+        button_exit.grid(row=0, column=3, padx=10, pady=10)
 
         # Make Entry NIK Read Only
         lock_nik_value = StringVar(value=entry_nik.get())
@@ -95,71 +95,149 @@ def add_identity():             # Input Nama, Mobil, Harga, Tanggal Masuk, Statu
         lock_entry_nik.grid(row=0, column=1, padx=30, pady=10)
         
         # create a label nama + entry nama
-        label_nama = Label(bottom_frame, text="Nama", bg='grey')
+        label_nama = Label(bottom_frame, text="Nama", bg='grey', font=("Helvetica", 20))
         label_nama.grid(row=0, column=1, padx=10, pady=10)
 
         entry_nama = Entry(bottom_frame, width=30)
         entry_nama.grid(row=0, column=2, padx=10, pady=10)
 
         # create a label mobil + entry mobil
-        label_mobil = Label(bottom_frame, text="Mobil", bg='grey')
+        label_mobil = Label(bottom_frame, text="Mobil", bg='grey', font=("Helvetica", 20))
         label_mobil.grid(row=1, column=1, padx=10, pady=10)
 
         entry_mobil = Entry(bottom_frame, width=30)
         entry_mobil.grid(row=1, column=2, padx=10, pady=10)
 
         # create a label harga + entry harga
-        label_harga = Label(bottom_frame, text="Harga", bg='grey')
+        label_harga = Label(bottom_frame, text="Harga (Rupiah)", bg='grey', font=("Helvetica", 20))
         label_harga.grid(row=2, column=1, padx=10, pady=10)
 
         entry_harga = Entry(bottom_frame, width=30)
         entry_harga.grid(row=2, column=2, padx=10, pady=10)
 
         # create a label tanggal masuk + entry tanggal masuk
-        label_tanggal_masuk = Label(bottom_frame, text="Tanggal Masuk", bg='grey')
+        label_tanggal_masuk = Label(bottom_frame, text="Tanggal Masuk", bg='grey', font=("Helvetica", 20))
         label_tanggal_masuk.grid(row=3, column=1, padx=10, pady=10)
 
-        entry_tanggal_masuk = Entry(bottom_frame, width=30)
+        entry_tanggal_masuk = DateEntry(bottom_frame, width=30, background='darkblue', foreground='white', borderwidth=2)
         entry_tanggal_masuk.grid(row=3, column=2, padx=10, pady=10)
 
         # create a label status + entry status
-        label_status = Label(bottom_frame, text="Status", bg='grey')
+        label_status = Label(bottom_frame, text="Status", bg='grey', font=("Helvetica", 20))
         label_status.grid(row=5, column=1, padx=10, pady=10)
 
         entry_status = Entry(bottom_frame, width=30)
         entry_status.grid(row=5, column=2, padx=10, pady=10)
 
         # create a label tanggal keluar + entry tanggal keluar
-        label_tanggal_keluar = Label(bottom_frame, text="Tanggal Keluar", bg='grey')
+        label_tanggal_keluar = Label(bottom_frame, text="Tanggal Keluar", bg='grey', font=("Helvetica", 20))
         label_tanggal_keluar.grid(row=4, column=1, padx=10, pady=10)
 
-        entry_tanggal_keluar = Entry(bottom_frame, width=30)
+        entry_tanggal_keluar = DateEntry(bottom_frame, width=30, background='darkblue', foreground='white', borderwidth=2)
         entry_tanggal_keluar.grid(row=4, column=2, padx=10, pady=10)
 
         # add buttons to the bottom frame
-
-        button_add = Button(bottom_frame, text="Add", width=30, height=1, command=get_entry_data)
+        button_add = Button(bottom_frame, text="Add", width=30, height=1, command=add_get_entry_data)
         button_add.grid(row=6, column=1, padx=8, pady=10, sticky='w')
 
-def get_entry_data():
+def add_get_entry_data():
         global entry_nik, entry_nama, entry_mobil, entry_harga, entry_tanggal_masuk, entry_status, entry_tanggal_keluar
         data_nik = entry_nik.get()
         data_nama = entry_nama.get()
         data_mobil = entry_mobil.get()
         data_harga = entry_harga.get()
-        data_tanggal_masuk = entry_tanggal_masuk.get()
+        data_tanggal_masuk = entry_tanggal_masuk.get_date()
         data_status = entry_status.get()
-        data_tanggal_keluar = entry_tanggal_keluar.get()
+        data_tanggal_keluar = entry_tanggal_keluar.get_date()
         result = messagebox.askyesno(title="Confirmation", message="Apakah anda yakin ingin menambahkan data ini?")
         if result:
-                ft.add_data(data_nik, data_nama, data_mobil, data_harga, data_tanggal_masuk, data_status, data_tanggal_keluar)
                 pesan = f"Data dengan NIK {data_nik} berhasil ditambahkan"
                 showinfo(title="Success",message=pesan)
+                ft.add_data(data_nik, data_nama, data_mobil, data_harga, data_tanggal_masuk, data_status, data_tanggal_keluar)
                 add_menu()
         else:
                 add_menu()
 
-def show_menu():                # Show All Data, Show By Primary Key, dan Exit
+def show_menu():                
+        global top_frame, bottom_frame, button_show_all, button_show_by_nik, button_exit
+        
+        top_frame.grid_forget()
+        bottom_frame.grid_forget()
+
+        top_frame = Frame(root, width=1175, height=500, bg='grey')
+        top_frame.grid(row=1, column=0, padx=50, pady=50)
+
+        # add 3 buttons to the bottom frame
+        button_show_all = Button(top_frame, text="Show All", width=30, height=3, command=show_get_length)
+        button_show_all.grid(row=0, column=0, padx=480, pady=50)
+
+        button_show_by_nik = Button(top_frame, text="Show By NIK", width=30, height=3, command=show_by_nik)
+        button_show_by_nik.grid(row=1, column=0, padx=480, pady=50)
+
+        button_exit = Button(top_frame, text="Exit", width=30, height=3, command=transition)
+        button_exit.grid(row=2, column=0, padx=480, pady=50)
+
+def show_get_length():                
+        ft.show_validate()
+        if ft.show_validate() == False:
+                pesan = "Data masih kosong silahkan tambah data terlebih dahulu"
+                showinfo(title="Warning",message=pesan)
+                pass
+        else:
+                button_show_all.grid_forget()
+                button_show_by_nik.grid_forget()
+                button_exit.grid_forget()
+                show_all()
+
+def show_all():                 
+        global top_frame
+
+        button_exit = Button(top_frame, text="Exit", width=30, height=3, command=show_menu)
+        button_exit.grid(row=2, column=0, padx=480, pady=50)
+
+        # create a new frame inside the right frame
+        tree_frame = Frame(top_frame, width=1100, height=500, bg='grey')
+        tree_frame.grid(row=0, column=0, padx=10, pady=50)  
+
+        # create a new scrollbar widget
+        tree_scroll = Scrollbar(tree_frame)
+        tree_scroll.pack(side=RIGHT, fill=Y)
+
+        # Create a Treeview widger and add it to the new frame
+        my_tree = ttk.Treeview(tree_frame)
+        my_tree.pack(side=LEFT, fill=BOTH, expand=True)
+
+        # configure the scrollbar to scroll the Treeview widget
+        my_tree.configure(yscrollcommand=tree_scroll.set)
+        tree_scroll.configure(command=my_tree.yview)
+
+        # add columns to the Treeview widget
+        my_tree['columns'] = ('NIK', 'Name', 'Car', 'DateIn', 'DateOut', 'Price', 'Status')
+
+        # set column headings
+        my_tree.heading('#0', text='No')
+        my_tree.column('#0', width=50)
+        my_tree.heading('NIK', text='NIK')
+        my_tree.column('NIK', width=150)
+        my_tree.heading('Name', text='Name')
+        my_tree.column('Name', width=150)
+        my_tree.heading('Car', text='Car')
+        my_tree.column('Car', width=150)
+        my_tree.heading('DateIn', text='Date In')
+        my_tree.column('DateIn', width=150)
+        my_tree.heading('DateOut', text='Date Out')
+        my_tree.column('DateOut', width=150)
+        my_tree.heading('Price', text='Harga (Rupiah)')
+        my_tree.column('Price', width=150)
+        my_tree.heading('Status', text='Status')
+        my_tree.column('Status', width=100)
+
+        ft.show_full_data(my_tree)
+
+def show_by_nik():             
+        print("Show By NIK")
+
+def edit_menu():                
         global top_frame, bottom_frame
         
         top_frame.grid_forget()
@@ -168,7 +246,7 @@ def show_menu():                # Show All Data, Show By Primary Key, dan Exit
         mid_frame = Frame(root, width=1175, height=500, bg='grey')
         mid_frame.grid(row=1, column=0, padx=50, pady=50)
 
-def edit_menu():                # Input Primary Key, Edit Data, dan Exit
+def delete_menu():             
         global top_frame, bottom_frame
         
         top_frame.grid_forget()
@@ -177,16 +255,7 @@ def edit_menu():                # Input Primary Key, Edit Data, dan Exit
         mid_frame = Frame(root, width=1175, height=500, bg='grey')
         mid_frame.grid(row=1, column=0, padx=50, pady=50)
 
-def delete_menu():              # Input Primary Key, Delete Data, dan Exit
-        global top_frame, bottom_frame
-        
-        top_frame.grid_forget()
-        bottom_frame.grid_forget()
-
-        mid_frame = Frame(root, width=1175, height=500, bg='grey')
-        mid_frame.grid(row=1, column=0, padx=50, pady=50)
-
-def exit_menu():                # Exit
+def exit_menu():                
         root.destroy()
 
 if __name__ == '__main__':
